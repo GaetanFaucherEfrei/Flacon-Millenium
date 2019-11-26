717f6633a3e2729de16a8ab9a1985ba572c25197
Etienne <etienne.robbiani@gmail.com>
2019-11-26 17:41:29 +0100

    add route

---
+++ FlaconMilenium/routes/bouteille.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const Bouteille = require('../models/bouteille.model.js')
+const mongoose = require('mongoose')
+var router = express.Router()
+
+global.results = null
+
+router.post('/add', async (req, res) => {
+
+})
+
+router.get('/view', async (req, res) => {
+
+})
+
+module.exports = router
