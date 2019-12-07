5e7ba07e869551d52536ea545e33f53f7b8fae6f
Etienne <etienne.robbiani@gmail.com>
2019-12-07 19:05:27 +0100

    added size and dosage

---
+++ FlaconMillenium/public/js/sugarDosageFonctions.js
@@ -0,0 +1,68 @@
+
+function createSugarDosageForm (fonction, path, id, name, dosage, description) {
+  document.getElementById('editorSugarDosageForm').innerHTML = '<div class="form-popup" id="mySugarDosageForm">' +
+      '<form class="form-container" >' +
+      '  <h1>Edit</h1>' +
+      '  <div class="form-input">' +
+      '    <label for="name"><b>Name</b></label>' +
+      '    <input type="text" value="' + name + '" name="name" required="required" />' +
+      '    <br />' +
+      '    <label for="dosage"><b>Dosage</b></label>' +
+      '    <input type="Number" value="' + dosage + '" name="dosage" />' +
+      '    <br />' +
+      '    <label for="description"><b>Description</b></label>' +
+      '    <input type="text" value="' + description + '" name="description" />' +
+      '  </div>' +
+      '  <br />' +
+      '  <button type="button" class="btn" onclick="' + fonction + '(\'' + path + '\',\'' + id + '\')">Submit</button> </br>' +
+      '  <button type="button" class="btn cancel" onclick="closeForm(\'editorSugarDosageForm\')">Cancel</button>' +
+      '  </form>' +
+      '</div>'
+}
+
+function postSugarDosageById (path, id) {
+  $.ajax({
+    type: 'POST',
+    url: path,
+    data: {
+      id: id,
+      name: document.querySelectorAll('#mySugarDosageForm > form > div > input[name="name"]')[0].value,
+      dosage: document.querySelectorAll('#mySugarDosageForm > form > div > input[name="dosage"]')[0].value,
+      description: document.querySelectorAll('#mySugarDosageForm > form > div > input[name="description"]')[0].value
+    },
+    success: function (answer, status) {
+      console.log('success : ' + status)
+      closeForm('editorSugarDosageForm')
+      window.location.reload()
+    },
+    error: function (answer, status, error) {
+      console.log('success : ' + status)
+      window.alert(answer.responseText)
+    },
+    complete: function (answer, status) {}
+  })
+}
+
+function patchSugarDosageById (path, id) {
+  $.ajax({
+    type: 'PATCH',
+    url: path,
+    data: {
+      id: id,
+      name: document.querySelectorAll('#mySugarDosageForm > form > div > input[name="name"]')[0].value,
+      dosage: document.querySelectorAll('#mySugarDosageForm > form > div > input[name="dosage"]')[0].value,
+      description: document.querySelectorAll('#mySugarDosageForm > form > div > input[name="description"]')[0].value
+    },
+    success: function (answer, status) {
+      console.log('success : ' + status)
+      console.log('success answer : ' + answer.name)
+      closeForm('editorSugarDosageForm')
+      window.location.reload()
+    },
+    error: function (answer, status, error) {
+      console.log('Error : ' + error)
+      window.alert(answer.responseText)
+    },
+    complete: function (answer, status) {}
+  })
+}
---
+++ FlaconMillenium/routes/bdd/sugarDosage.js
@@ -0,0 +1,204 @@
+const EXPRESS = require('express')
+const SUGARDOSAGE = require('../../models/sugarDosage.model.js')
+var Router = EXPRESS.Router()
+
+global.results = null
+
+/* ERROR CODE : https://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html
+  200 : OK
+  206 : Partial Content
+
+  client:
+    400 : Bad Request
+    401 : Unauthorized
+    403 : Forbidden
+    404 : Not Found
+
+  server:
+    500 : Internal Server Error
+    520 : Unknown Error
+*/
+
+Router.get('/list', async (req, res) => {
+  try {
+    if (req.user) {
+      SUGARDOSAGE.find({}, function (error, result) {
+        if (error) {
+          // console.log('The sugarDosage did not exist.')
+          notFound(req, res, 0)
+        } else {
+          // console.log('Result: ', result)
+          var alert = req.session.alert
+          req.session.alert = ''
+
+          res.status(200).render('sugarDosage/sugarDosageList', { data: result, name: req.user.username, alert: alert })
+          // res.send(JSON.stringify(result))
+        }
+      }).sort({ _id: -1 })
+    } else {
+    // console.log('User not identified.')
+      req.session.oldUrl = req.originalUrl
+      res.status(401).redirect('/user/login')
+    }
+  } catch (error) {
+    console.log('There was an error with our server : ' + error)
+    res.status(403).send('There was an error with our server.')
+  }
+})
+
+Router.get('/unit', async (req, res) => {
+  try {
+    if (req.user) {
+      if (typeof req.query.id === 'undefined') {
+        // console.log('The sugarDosage did not exist.')
+        notFound(req, res, 0)
+      } else {
+        SUGARDOSAGE.findById({ _id: req.query.id }, function (error, result) {
+          if (error) {
+            // console.log('The sugarDosage did not exist.')
+            notFound(req, res, 1)
+          } else {
+            if (result) {
+              res.format({
+                'text/html': function () {
+                  res.status(200).render('sugarDosage/sugarDosageView', { data: result, name: req.user.username })
+                },
+
+                'application/json': function () {
+                  res.status(200).send(result)
+                },
+
+                default: function () {
+                  res.status(200).render('sugarDosage/sugarDosageView', { data: result, name: req.user.username })
+                }
+              })
+            } else {
+              notFound(req, res, 1)
+            }
+          }
+        })
+      }
+    } else {
+    // console.log('User not identified.')
+      req.session.oldUrl = req.originalUrl
+      res.status(401).redirect('/user/login')
+    }
+  } catch (error) {
+    console.log('There was an error with our server : ' + error)
+    res.status(520).send('There was an error with our server.')
+  }
+})
+
+// to use with an ajax call
+Router.post('/unit', async (req, res) => {
+  try {
+    if (req.user) {
+      new SUGARDOSAGE({
+        name: req.body.name,
+        dosage: req.body.dosage,
+        description: req.body.description
+      }).save(function (error, result) {
+        if (error) {
+          console.log('Check the type of your entry :' + error)
+          res.status(400).send('Check the type of your entry.')
+        } else {
+          // console.log('The post was succesfull.')
+          res.status(200).send(result)
+        }
+      })
+    } else {
+    // console.log('User not identified.')
+      req.session.oldUrl = '/sugarDosage/list'
+      res.status(401).redirect('/user/login')
+    }
+  } catch (error) {
+    console.log('There was an error with our server : ' + error)
+    res.status(520).send('There was an error with our server.')
+  }
+})
+
+// to use with an ajax call
+Router.patch('/unit', async (req, res) => {
+  try {
+    if (req.user) {
+      SUGARDOSAGE.findByIdAndUpdate({ _id: req.body.id }, {
+        $set: {
+          name: req.body.name,
+          dosage: req.body.dosage,
+          description: req.body.description
+        }
+      }, function (error, result) {
+        if (error) {
+          // console.log('The sugarDosage did not exist.')
+          notFound(req, res, 1)
+        } else {
+          if (result) {
+            result.name = req.body.name
+            result.dosage = req.body.dosage
+            result.description = req.body.description
+
+            // console.log('The post was succesfull.')
+            res.status(200).send(result)
+          } else {
+            notFound(req, res, 1)
+          }
+        }
+      })
+    } else {
+      // console.log('User not identified.')
+      req.session.oldUrl = req.originalUrl + '?id=' + req.params.identification
+      res.status(401).redirect('/user/login')
+    }
+  } catch (error) {
+    console.log('There was an error with our server : ' + error)
+    res.status(520).send('There was an error with our server.')
+  }
+})
+
+// to use with an ajax call
+Router.delete('/unit', async (req, res) => {
+  try {
+    if (req.user) {
+      SUGARDOSAGE.findByIdAndDelete({ _id: req.body.id }, function (error, result) {
+        if (error) {
+          // console.log('The sugarDosage did not exist.')
+          notFound(req, res, 1)
+        } else {
+          // console.log('The delete was succesfull.')
+          res.status(200).send(result)
+        }
+      })
+    } else {
+    // console.log('User not identified.')
+      req.session.oldUrl = req.originalUrl + '?id=' + req.params.identification
+      res.status(401).redirect('/user/login')
+    }
+  } catch (error) {
+    console.log('There was an error with our server : ' + error)
+    res.status(520).send('There was an error with our server.')
+  }
+})
+
+function notFound (req, res, alert) {
+  res.format({
+    'text/html': function () {
+      if (alert) {
+        req.session.alert = 'Error : The sugarDosage was not found.'
+      }
+      res.status(404).redirect('/home')
+    },
+
+    'application/json': function () {
+      res.status(404).send('Error : The sugarDosage was not found.')
+    },
+
+    default: function () {
+      if (alert) {
+        req.session.alert = 'Error : The sugarDosage was not found.'
+      }
+      res.status(404).redirect('/home')
+    }
+  })
+}
+
+module.exports = Router
