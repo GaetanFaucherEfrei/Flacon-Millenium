4bc23e0b4c16aaee295ae1522a905481dd87e6b7
Mistgan <kevin.fontaine@efrei.net>
2020-01-17 00:24:18 +0100

    Multiple Channel Forum
    Create multiple channel for forum
    and each channel is independant

--- FlaconMillenium/models/comment.model.js
+++ FlaconMillenium/models/comment.model.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 var commentSchema = new Schema({
+  topic: { type: String, required: true },
   content: { type: String, required: true },
   author: { type: String, required: true },
   date: { type: Date, required: true }
